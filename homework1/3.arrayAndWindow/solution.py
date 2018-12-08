a5b05e141ab2288662c62e90cf73791305d23921
Lemonstars <mf1832103@smail.nju.edu.cn>
2018-12-08 19:16:30 +0800

    [滑动窗口最大值之和]

--- homework1/3.arrayAndWindow/solution.py
+++ homework1/3.arrayAndWindow/solution.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*
 # Description
-# 给定一个整型数组arr和一个大小为w的窗口，窗口从数组最左边滑动到最右边，每次向右滑动一个位置，求出每一次滑动时窗口内最大元素的和。
+# 给定一个整型数组arr和一个大小为w的窗口，窗口从数组最左边滑动到最右边，
+# 每次向右滑动一个位置，求出每一次滑动时窗口内最大元素的和。
 # Input
 # 输入的第一行为数组，每一个元素使用空格隔开；第二行为窗口大小。
 # Output
@@ -9,3 +11,30 @@
 # 3
 # Sample Output 1
 # 27
+
+from collections import deque
+
+# input: data & width
+data = list()
+arr = input()
+num = arr.split(' ')
+for item in num:
+    data.append(int(item))
+width = int(input())
+
+# process
+res = 0
+max_queue = deque()
+for index in range(len(data)):
+    item = data[index]
+    while max_queue and item >= data[max_queue[-1]]:
+        max_queue.pop()
+    max_queue.append(index)
+
+    while max_queue and max_queue[0] <= index - width:
+        max_queue.popleft()
+
+    if index >= width-1:
+        res += data[max_queue[0]]
+
+print(res)
