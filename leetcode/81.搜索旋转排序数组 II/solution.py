a9301d86ffb00a75eeededc7fda5764d2bba7e03
Lemonstars <mf1832103@smail.nju.edu.cn>
2019-05-06 16:10:16 +0800

    [leetcode]81.搜索旋转排序数组 II

---
+++ leetcode/81.搜索旋转排序数组 II/solution.py
@@ -0,0 +1,51 @@
+# 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+# ( 例如，数组 [0,0,1,2,2,5,6] 可能变为 [2,5,6,0,0,1,2] )。
+# 编写一个函数来判断给定的目标值是否存在于数组中。若存在返回 true，否则返回 false。
+#
+# 示例 1:
+# 输入: nums = [2,5,6,0,0,1,2], target = 0
+# 输出: true
+#
+# 示例 2:
+# 输入: nums = [2,5,6,0,0,1,2], target = 3
+# 输出: false
+# 进阶:
+#
+# 这是 搜索旋转排序数组 的延伸题目，本题中的 nums  可能包含重复元素。
+# 这会影响到程序的时间复杂度吗？会有怎样的影响，为什么？
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        if len(nums) == 0:
+            return False
+
+        lo, hi = 0, len(nums) - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if nums[mid] == target:
+                return True
+
+            if nums[lo] < nums[mid]:
+                if nums[lo] <= target < nums[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            elif nums[lo] > nums[mid]:
+                if nums[mid] < target <= nums[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+            else:
+                if nums[lo] != nums[hi]:
+                    lo = mid + 1
+                else:
+                    lo += 1
+                    hi -= 1
+
+        return False
+
+
+s = Solution()
+print(s.search([1, 3, 1, 1, 1], 3))
