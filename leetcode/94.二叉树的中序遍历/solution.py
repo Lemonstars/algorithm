64623002a754244d6c66af6e9186e9c772568c6c
星夜 <john.lx@antfin.com>
2019-05-28 19:35:58 +0800

    [leetcode]94.二叉树的中序遍历

---
+++ leetcode/94.二叉树的中序遍历/solution.py
@@ -0,0 +1,45 @@
+# 给定一个二叉树，返回它的中序 遍历。
+#
+# 示例:
+#
+# 输入: [1,null,2,3]
+#    1
+#     \
+#      2
+#     /
+#    3
+#
+# 输出: [1,3,2]
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res, stack = [], []
+
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+
+            root = stack.pop()
+            res.append(root.val)
+            root = root.right
+
+        return res
+
+
+node1 = TreeNode(1)
+node2 = TreeNode(2)
+node3 = TreeNode(3)
+node1.right = node2
+node2.left = node3
+s = Solution()
+print(s.inorderTraversal(node1))
