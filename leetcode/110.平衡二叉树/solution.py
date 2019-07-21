ec23be84dea2ec55770faeb2aa4f59bddf6a3c25
Lemonstars <mf1832103@smail.nju.edu.cn>
2019-07-21 22:00:58 +0800

    [leetcode]110.平衡二叉树

---
+++ leetcode/110.平衡二叉树/solution.py
@@ -0,0 +1,61 @@
+# 给定一个二叉树，判断它是否是高度平衡的二叉树。
+#
+# 本题中，一棵高度平衡二叉树定义为：
+# 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+#
+# 示例 1:
+#
+# 给定二叉树 [3,9,20,null,null,15,7]
+#
+#     3
+#    / \
+#   9  20
+#     /  \
+#    15   7
+# 返回 true 。
+#
+# 示例 2:
+#
+# 给定二叉树 [1,2,2,3,3,null,null,4,4]
+#
+#        1
+#       / \
+#      2   2
+#     / \
+#    3   3
+#   / \
+#  4   4
+# 返回 false 。
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+
+    def isBalanced(self, root: TreeNode) -> bool:
+
+        res = True
+
+        def depth(node: TreeNode) -> int:
+            nonlocal res
+            if node is None:
+                return 0
+
+            lh = depth(node.left) + 1
+            rh = depth(node.right) + 1
+            if abs(lh - rh) > 1:
+                res = False
+
+            return max(lh, rh)
+
+        if root is None:
+            return True
+
+        depth(root)
+        return res
